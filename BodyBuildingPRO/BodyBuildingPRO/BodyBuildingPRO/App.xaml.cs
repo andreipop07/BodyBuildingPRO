9f58d1c70b3ea47604f5916e8ab77ddf9e31ba68
Pop Andrei <andreipop07@yahoo.com>
2021-03-31 16:53:39 +0300

    Adding login/home page

--- BodyBuildingPRO/BodyBuildingPRO/BodyBuildingPRO/App.xaml.cs
+++ BodyBuildingPRO/BodyBuildingPRO/BodyBuildingPRO/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BodyBuildingPRO.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,8 @@ namespace BodyBuildingPRO
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new Login());
+            
         }
 
         protected override void OnStart()
---
+++ BodyBuildingPRO/BodyBuildingPRO/BodyBuildingPRO/Views/Login.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BodyBuildingPRO.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        private async void Button_Signin(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Home());
+        }
+    }
+}
