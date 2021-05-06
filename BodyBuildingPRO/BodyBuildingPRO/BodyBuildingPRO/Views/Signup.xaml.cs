a5a6f8316484eac6d3546dbbc78631bd32ba7c2a
Darau Alex <daraualexandru@gmail.com>
2021-05-06 15:05:32 +0300

    Validate email and password

--- BodyBuildingPRO/BodyBuildingPRO/BodyBuildingPRO/Views/Signup.xaml.cs
+++ BodyBuildingPRO/BodyBuildingPRO/BodyBuildingPRO/Views/Signup.xaml.cs
@@ -24,7 +24,8 @@ namespace BodyBuildingPRO.Views
         }
 
         private async void Button_signup(object sender, EventArgs e)
-        {
+        {   
+           
             FirebaseClient firebase = new FirebaseClient("https://bodybuilding-f3671-default-rtdb.firebaseio.com/");
 
             var person = await firebaseHelper.GetPerson(emaill.Text);
@@ -32,13 +33,55 @@ namespace BodyBuildingPRO.Views
             if (person != null)
             {
                 await DisplayAlert("Fail", "This username already exists", "OK");
-                password1.Text = "";
-                emaill.Text = "";
             }
-            await firebase
-                .Child("User")
-                .PostAsync(new User() { Name = emaill.Text, Password = password1.Text });
 
+            if (ValidateEmailAndPassword() && person == null)
+            {
+                        await firebase
+                            .Child("User")
+                            .PostAsync(new User() { Name = emaill.Text, Password = password1.Text });
+                            await DisplayAlert("Succes", "Registration succesfull", "OK");
+                            await Navigation.PushAsync(new Login());
+            }
+         }
+
+
+        bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private bool ValidateEmailAndPassword()
+        {
+            bool IsValid = true;
+
+            if (!IsValidEmail(emaill.Text))
+            {
+                DisplayAlert("Fail", "The email is invalid", "OK");
+                password1.Text = "";
+                password2.Text = "";
+                emaill.Text = "";
+                IsValid = false;
+            }
+
+            if (password1.Text != password2.Text)
+            {
+                DisplayAlert("Fail", "The password are not the same", "OK");
+                password1.Text = "";
+                password2.Text = "";
+                emaill.Text = "";
+                IsValid = false;
+            }
+
+            return IsValid;
         }
     }
 
